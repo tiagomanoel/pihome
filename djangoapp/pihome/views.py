626370968baee5f8ba7fa635fddbb9c68a697456
Tiago Giglia Manoel <tiagomanoel@tiagomanoel.com.br>
2025-03-06 13:42:28 -0300

    connect in db

--- djangoapp/pihome/views.py
+++ djangoapp/pihome/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from pihome.models import Action
+from pihome.serializers import ActionSerializer
+
 
 def index(request):
     return render(request, 'pihome/index.html')
